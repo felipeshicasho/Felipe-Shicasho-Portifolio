4273e256e80c47560726ea054b0fad306c5a23d7
felipeshicasho <felipetoleds@gmail.com>
2024-09-17 10:52:09 -0300

    Ajustando o Portifólio no profile.json e ligando elas ao Front da página

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,32 @@ function updateHardSkills(profileData){
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"</li>`).join('')   
 }
 
+function updateLanguages(profileData){
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')   
+}
+
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById("profile.portfolio")
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+              <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+              <a
+                href="${project.url}"
+                target="_blank"
+                >${project.url}</a
+              >
+        </li>
+        `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
 })()
