68db30b8ba6d68894c1184133e7ac010600e1a4a
felipeshicasho <felipetoleds@gmail.com>
2024-09-17 10:27:56 -0300

    Ajustando as SoftSkill e HardSkill no profile.json e ligando elas ao Front da página

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/felipeshicasho/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/felipeshicasho/Felipe-Shicasho-Portifolio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
