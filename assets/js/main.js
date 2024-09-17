d9f341deecca0721742c5dffccd2d44a05e9f538
felipeshicasho <felipetoleds@gmail.com>
2024-09-17 09:54:19 -0300

    Corrigindo o JSON / Preenchendo os dados do Perfil com os dados que estão no profile.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
+function updateProfileInfo(profileData){ 
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `https://wa.me//${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `malito:${profileData.email}`
+
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log()
-})
+    updateProfileInfo(profileData)
+})()
